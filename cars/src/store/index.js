35e976055353ffa6a206ecfb06db892f6fe23c67
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-05-23 15:50:23 -0300

    redux store done

---
+++ cars/src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { carsReducer, addCar, removeCar, changeTerm } from "./slices/carSlice";
+import { formReducer, changeName, changeCost } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+
+export { store, changeName, changeCost, addCar, removeCar, changeTerm };
